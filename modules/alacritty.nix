0b84e4a80366c974bbcc4d147fdcf16a75c4c0be
es-sai-fi <athousandheavenlycats@protonmail.com>
2026-01-21 18:21:45 -0500

    wrappers/alacritty: simplify wrapping, there's no need of making a dir and a symlink for something that can be done with just a flag that points to the generated config in the nix store

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -44,21 +44,11 @@ in {
     { options, inputs }:
     let
       generator = inputs.nixpkgs.pkgs.formats.toml {};
+      generatedConfig = options.configFile or (generator.generate "alacritty.toml" options.settings);
     in
     assert !(options ? settings && options ? configFile);
-      inputs.mkWrapper {
-        inherit (options) package;
-        preWrap = ''
-          mkdir -p $out/alacritty
-        '';
-        symlinks = {
-          "$out/alacritty/alacritty.toml" =
-            if options ? configFile
-              then options.configFile
-            else if options ? settings
-              then generator.generate "alacritty.toml" options.settings
-            else null;
-        };
-        flags = ["--config-file" "$out/alacritty/alacritty.toml"];
-      };
+    inputs.mkWrapper {
+      inherit (options) package;
+      flags = ["--config-file" "${generatedConfig}"];
+    };
 }
